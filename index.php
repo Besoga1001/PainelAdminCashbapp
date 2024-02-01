f6b9417a22a268ce57b4bc5ebac61552930ff691
Besoga1001 <bernardo.sousa.garcia@gmail.com>
2024-01-31 22:41:21 -0300

    Create Grid in index Page

--- index.php
+++ index.php
@@ -34,52 +34,53 @@
     <title>Painel de Relatórios</title>
 </head>
 <body>
-    <div class="header">
-        <h1 class="logoText">CASHBAPP</h1>
-        <img id="logo" src="images/LogoCashbapp.png">
-        <h1 class="title">PAINEL DE ADMINISTRADOR</h1>
-    </div>
-    <div class="break"></div>
-    <div class="menuBar">
-        <div class="firstComponentMenuBar">
-            <img class="iconMenuBar" src="images/solicitacaoImg.png">
-            <h1 class="textMenuBar">SOLICITAÇÕES</h1>
-            <img class="lineMenuBar" src="images/division-menuBar.png">
+    <div class="container">
+        <div class="header">
+            <h1 class="logoText">CASHBAPP</h1>
+            <img id="logo" src="images/LogoCashbapp.png">
+            <h1 class="title">PAINEL DE ADMINISTRADOR</h1>
         </div>
-        <div class="componentMenuBar">
-            <img class="iconMenuBar" src="images/monetarioIcon.png">
-            <h1 class="textMenuBar">MONETÁRIO</h1>
-            <img class="lineMenuBar" src="images/division-menuBar.png">
+        <div class="break"></div>
+        <div class="menuBar">
+            <div class="firstComponentMenuBar">
+                <img class="iconMenuBar" src="images/solicitacaoImg.png">
+                <h1 class="textMenuBar">SOLICITAÇÕES</h1>
+                <img class="lineMenuBar" src="images/division-menuBar.png">
+            </div>
+            <div class="componentMenuBar">
+                <img class="iconMenuBar" src="images/monetarioIcon.png">
+                <h1 class="textMenuBar">MONETÁRIO</h1>
+                <img class="lineMenuBar" src="images/division-menuBar.png">
+            </div>
         </div>
-    </div>
-    <div id="realeases">
-        <table>
-            <caption>SOLICITAÇÕES</caption>
-            <tr>
-                <th>ID</th>
-                <th id="seller">VENDEDOR</th>
-                <th id="situation">SITUAÇÃO</th>
-                <th id="situation">ARQUIVO PDF</th>
-            </tr>
-            
-                <?php
-                    $result = $connection->query("SELECT * FROM realeases")->fetchAll();
+        <div id="realeases">
+            <table>
+                <caption>SOLICITAÇÕES</caption>
+                <tr>
+                    <th>ID</th>
+                    <th id="seller">VENDEDOR</th>
+                    <th id="situation">SITUAÇÃO</th>
+                    <th id="situation">ARQUIVO PDF</th>
+                </tr>
+                    <?php
+                        $result = $connection->query("SELECT * FROM realeases")->fetchAll();
 
-                    foreach($result as $item) {
-                        $id = strtoupper($item['id']);
-                        $userName = mb_strtoupper($item['userName']);
-                        $approved = getStringApproved($item['approved']);
-                        $pdf = $item['nfe_pdf'];
-                        echo "<tr>";
-                            echo "<td>$id</td>";
-                            echo "<td>$userName</td>";
-                            echo "<td>$approved</td>";
-                            echo "<td>$pdf</td>";
-                        echo "</tr>";
-                    }
-                ?>
-            </tr>
-        </table>
+                        foreach($result as $item) {
+                            $id = strtoupper($item['id']);
+                            $userName = mb_strtoupper($item['userName']);
+                            $approved = getStringApproved($item['approved']);
+                            $pdf = $item['nfe_pdf'];
+                            echo "<tr>";
+                                echo "<td>$id</td>";
+                                echo "<td>$userName</td>";
+                                echo "<td>$approved</td>";
+                                echo "<td>$pdf</td>";
+                            echo "</tr>";
+                        }
+                    ?>
+                </tr>
+            </table>
+        </div>
     </div>
 </body>
 </html>
